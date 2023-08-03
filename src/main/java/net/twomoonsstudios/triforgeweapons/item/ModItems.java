180ee1833ad10ebd8533c9bfa14ceb740016dc61
Hornster <karol199714@gmail.com>
2023-08-03 20:10:22 +0200

    Added weapon models and textures.

---
+++ src/main/java/net/twomoonsstudios/triforgeweapons/item/ModItems.java
@@ -0,0 +1,12 @@
+package net.twomoonsstudios.triforgeweapons.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.twomoonsstudios.triforgeweapons.TriforgeWeapons;
+
+public class ModItems {
+    public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TriforgeWeapons.MOD_ID);
+
+
+}
