c06d8902e6c9f1d14402269344a4a4126ba9b2de
Hornster <karol199714@gmail.com>
2023-08-28 19:58:30 +0200

    (4) Added classes for all weapons.

---
+++ src/main/java/net/twomoonsstudios/moonsweaponry/item/weapons/HammerWeapon.java
@@ -0,0 +1,10 @@
+package net.twomoonsstudios.moonsweaponry.item.weapons;
+
+import net.twomoonsstudios.moonsweaponry.config.MoonsWeaponsConfig;
+import net.twomoonsstudios.moonsweaponry.item.TriforgeTieredWeapon;
+
+public class HammerWeapon extends TriforgeTieredWeapon {
+    public HammerWeapon(MoonsWeaponsConfig.WeaponInfo stats) {
+        super(stats);
+    }
+}
---
+++ src/main/java/net/twomoonsstudios/moonsweaponry/item/weapons/WarglaiveWeapon.java
@@ -0,0 +1,10 @@
+package net.twomoonsstudios.moonsweaponry.item.weapons;
+
+import net.twomoonsstudios.moonsweaponry.config.MoonsWeaponsConfig;
+import net.twomoonsstudios.moonsweaponry.item.TriforgeTieredWeapon;
+
+public class WarglaiveWeapon extends TriforgeTieredWeapon {
+    public WarglaiveWeapon(MoonsWeaponsConfig.WeaponInfo stats) {
+        super(stats);
+    }
+}
