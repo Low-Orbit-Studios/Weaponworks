af9e671e206328a7c940f53b22401928ff726512
Hornster <karol199714@gmail.com>
2023-10-12 20:26:46 +0200

    (#9) Fixed durability on hammers

--- src/main/java/net/twomoonsstudios/moonsweaponry/item/TriforgeTieredItem.java
+++ src/main/java/net/twomoonsstudios/moonsweaponry/item/TriforgeTieredItem.java
@@ -2,11 +2,19 @@ package net.twomoonsstudios.moonsweaponry.item;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
 import net.twomoonsstudios.moonsweaponry.config.MoonsWeaponsConfig;
 
 public class TriforgeTieredItem extends TieredItem implements Vanishable {
@@ -33,5 +41,44 @@ public class TriforgeTieredItem extends TieredItem implements Vanishable {
     @Override
     public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(EquipmentSlot pEquipmentSlot) {
         return pEquipmentSlot == EquipmentSlot.MAINHAND ? this.defaultModifiers : super.getDefaultAttributeModifiers(pEquipmentSlot);
+    }
+
+    /**
+     * Current implementations of this method in child classes do not use the entry argument beside ev. They just raise
+     * the damage on the stack.
+     */
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+        pStack.hurtAndBreak(1, pAttacker, (event) -> {
+            event.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+        });
+        return true;
+    }
+
+    /**
+     * Called when a {@link net.minecraft.world.level.block.Block} is destroyed using this Item. Return {@code true} to
+     * trigger the "Use Item" statistic.
+     */
+    @Override
+    public boolean mineBlock(ItemStack pStack, Level pLevel, BlockState pState, BlockPos pPos, LivingEntity pEntityLiving) {
+        if (pState.getDestroySpeed(pLevel, pPos) != 0.0F) {
+            pStack.hurtAndBreak(2, pEntityLiving, (broken) -> {
+                broken.broadcastBreakEvent(EquipmentSlot.MAINHAND);
+            });
+        }
+
+        return true;
+    }
+    @Override
+
+    public boolean canAttackBlock(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer) {
+        return !pPlayer.isCreative();
+    }
+
+    @Override
+    public float getDestroySpeed(ItemStack pStack, BlockState pState) {
+        Material material = pState.getMaterial();
+        return material != Material.PLANT && material != Material.REPLACEABLE_PLANT && !pState.is(BlockTags.LEAVES) && material != Material.VEGETABLE ? 1.0F : 1.5F;
+
     }
 }
