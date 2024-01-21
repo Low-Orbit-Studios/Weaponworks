39a69b9570a5919e95420b9359b39800ad9900a7
Hornster <karol199714@gmail.com>
2024-01-21 20:21:16 +0100

    Configured fabric build. Still need to add dependencies and correct errors in classes tho.

---
+++ src/main/java/net/crazedaerialcable/weaponworks/WeaponworksClient.java
@@ -0,0 +1,10 @@
+package net.crazedaerialcable.weaponworks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeaponworksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/twomoonsstudios/moonsweaponry/config/objects/dummy/DummyWeaponConfig.java
@@ -0,0 +1,21 @@
+package net.twomoonsstudios.moonsweaponry.config.objects.dummy;
+
+import io.github.hornster.itemfig.api.serialization.config.ConfigObj;
+
+import java.lang.reflect.Type;
+
+public class DummyWeaponConfig extends ConfigObj {
+    /**
+     * @param myType needed for auto deserialization.
+     * @param myID   will be used to discern this config object from others during deserialization.
+     *               It MUST be unique. Just like in MC itself.
+     */
+    public DummyWeaponConfig(Type myType, String myID) {
+        super(myType, myID);
+    }
+
+    @Override
+    public void chkDefaultValues() {
+
+    }
+}
